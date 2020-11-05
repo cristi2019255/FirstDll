0eb8bf7b4d9837d9781514b73cae18decac49161
crm0236 <cristi2019255@gmial.com>
2020-11-05 13:22:06 +0200

    first

---
+++ SecondLibraryUiPath/Sum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel;
+
+namespace SecondLibraryUiPath
+{
+    public class Sum : CodeActivity
+    {
+        [Category("Input")]
+        int a { get; set; }
+
+        [Category("Input")]
+        int b { get; set; }
+
+        [Category("Output")]
+        int c { get; set; }
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            c = a + b;       
+        }
+    }
+}
